4b93c9244e560c24259dd6b206391b8671a2cee9
Priyanshu Gupta <priyanshugupta007007@gmail.com>
2026-09-18 15:28:36 +0530

    made deployment process start

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL:"http://localhost:8080/api"})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+});
 
-API.interceptors.request.use(config=>{
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-    const token = localStorage.getItem("token");
-
-    if(token){
-
-        config.headers.Authorization = `Bearer ${token}`
-    }
-
-    return config;
-})
-
-export default API;
+export default api;
